11579416a5e6f4292350374438270fe94f0bf7e4
Your Name <you@example.com>
2025-02-06 05:50:34 +0700

    error

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 from transformers import pipeline
 from contextlib import asynccontextmanager
 from datetime import datetime
+from fastapi.responses import JSONResponse
 from config import CLASIFICATION_MODEL, RECOGNITION_MODEL
 
 
@@ -35,21 +36,27 @@ def root():
 
 @app.post("/hijaiyah")
 def hijaiyah(secret: Annotated[str, Form()], file: UploadFile):
-    global whisper_clasification
-    audio = file.file.read()
-    startdate = datetime.now()
-    classifier = whisper_clasification(audio)
-    return {"result": classifier, "time": (datetime.now() - startdate)}
+    try:
+        global whisper_clasification
+        audio = file.file.read()
+        startdate = datetime.now()
+        classifier = whisper_clasification(audio)
+        return {"result": classifier, "time": (datetime.now() - startdate)}
+    except Exception as e:
+        return JSONResponse(status_code=212, content={"message": str(e)})
 
 
 @app.post("/quran")
 def quran(secret: Annotated[str, Form()], file: UploadFile):
-    global whisper_recognition
-    audio = file.file.read()
-    startdate = datetime.now()
-    segments, info = whisper_recognition.transcribe(BytesIO(audio), beam_size=5)
-    result = []
-    for segment in segments:
-        result.append({"start": segment.start, "end": segment.end, "text": segment.text})
+    try:
+        global whisper_recognition
+        audio = file.file.read()
+        startdate = datetime.now()
+        segments, info = whisper_recognition.transcribe(BytesIO(audio), beam_size=5)
+        result = []
+        for segment in segments:
+            result.append({"start": segment.start, "end": segment.end, "text": segment.text})
 
-    return {"result": result, "time": (datetime.now() - startdate)}
+        return {"result": result, "time": (datetime.now() - startdate)}
+    except Exception as e:
+        return JSONResponse(status_code=212, content={"message": str(e)})
